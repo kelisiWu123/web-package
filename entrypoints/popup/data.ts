b714fe7dfb376b69782ecd6796336467d96cc11f
吴必炫 <2506823133@qq.com>
2024-07-02 22:30:33 +0800

    feat: 新增一些我觉得有的收藏

--- entrypoints/popup/data.ts
+++ entrypoints/popup/data.ts
@@ -12,18 +12,19 @@ export const mockData:MockDataType[] = [
             {name: 'Ant-Mobile', iconName: 'antd.png', url: 'https://ant-design-mobile.antgroup.com/zh/guide/quick-start'},
             {name: 'Ant-ProComponents', iconName: 'antd.png', url: 'https://pro-components.antdigital.dev/components'},
             {name: 'Umi', iconName: 'antd.png', url: 'https://umijs.org/docs/guides/getting-started'},
+            {name: 'Dumi', iconName: 'antd.png', url: 'https://d.umijs.org/'},
             {name: '通义', iconName: 'antd.png', url: 'https://tongyi.aliyun.com/'},
+
         ]
     },
     {
-        name: 'react', father: true, children: [
+        name: 'react and utils', father: true, children: [
             {name: 'React', iconName: 'React.png', url: 'https://react.docschina.org/'},
-            {name: 'JS框架语法特性对比', iconName: 'React.png', url: 'https://component-party.lainbo.com/'},
-            {name: 'React-技术揭秘-卡颂', iconName: 'React.png', url: 'https://react.iamkasong.com/'},
             {name: 'Lodash', iconName: 'React.png', url: 'https://www.lodashjs.com/'},
             {name: 'AHooks', iconName: 'React.png', url: 'https://ahooks.js.org/zh-CN/'},
             {name: 'React-Use',iconName: 'React.png',url:'https://github.com/zenghongtu/react-use-chinese/blob/master/README.md'},
-            {name: 'Class and Function component',iconName: 'React.png',url: 'https://overreacted.io/how-are-function-components-different-from-classes/'}
+            {name: 'JS框架语法特性对比', iconName: 'React.png', url: 'https://component-party.lainbo.com/'},
+            {name: 'ES6 -书栈网', iconName: 'React.png',url:'https://www.bookstack.cn/read/es6-3rd/README.md'}
         ]
     },
     //https://github.com/type-challenges/type-challenges/blob/main/README.zh-CN.md
@@ -35,8 +36,17 @@ export const mockData:MockDataType[] = [
         ]
     },
     {
+        name: 'react好文', father: true, children: [
+            {name: '类组件和函数组件有什么不同?',iconName: 'React.png',url: 'https://overreacted.io/how-are-function-components-different-from-classes/'},
+            {name: '为什么钩子?',iconName: 'React.png',url: 'https://geekdaxue.co/read/garronge@tyarry/rz5yor'},
+            {name: 'React-技术揭秘-卡颂', iconName: 'React.png', url: 'https://react.iamkasong.com/'},
+        ]
+    },
+    {
         name: '前端代码规范', father: true, children: [
             {name: '状态管理规范', iconName: 'gf.png', url: 'https://jdf2e.github.io/jdc_fe_guide/docs/react/state/'},
+            {name: 'HTML标准', iconName: 'gf.png', url: 'https://html.spec.whatwg.org/multipage/'},
+            {name: 'PromiseA+', iconName: 'gf.png',url: 'https://promisesaplus.com/'}
         ]
     },
     {
@@ -51,7 +61,6 @@ export const mockData:MockDataType[] = [
             {name: '印记中文', iconName: 'wd.png', url: 'https://www.docschina.org/'},
         ]
     },
-
     {
         name: '开源项目趋势', father: true, children: [
             {name: 'Github Trending', iconName: 'github.png', url: 'https://github.com/trending'},
